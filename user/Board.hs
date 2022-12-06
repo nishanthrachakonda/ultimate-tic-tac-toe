28de101c25983807c31980bc5b9b433180d20595
Nishanth Rachakonda <32260842+NishanthRachakonda@users.noreply.github.com>
2022-12-05 18:51:22 -0800

    fixed winnning positions board

--- user/Board.hs
+++ user/Board.hs
@@ -3,6 +3,7 @@ module Board where
 import qualified Data.Map as Map
 import Grid
 import Utils
+import Data.Maybe
 
 -------------------------------
 ---- / Board
@@ -14,14 +15,16 @@ type Board = Map.Map Int (GridStatus, Grid)
 -------------------------------
 
 putb :: Board -> CurPos -> Value -> Status
-putb board pos value | state == Just Win X = Error
-                     | state == Just Win O = Error
-                     | state == Just Draw  = Error
-                     | otherwise           = putg grid (snd pos) value
+putb board pos value | state == Win X = Error
+                     | state == Win O = Error
+                     | state == Draw  = Error
+                     | otherwise      = putg grid (snd pos) value
                   where
                     gridT = Map.lookup (fst pos) board
-                    state = fst gridT
-                    grid  = snd gridT
+                    state | isNothing gridT = Ongoing
+                          | otherwise       = fst (fromJust gridT)
+                    grid  | isNothing gridT = Map.empty
+                          | otherwise       = snd (fromJust gridT)
 
 ----------------------------------
 ---- / State
@@ -36,24 +39,30 @@ isbWon :: Board -> Value -> Bool
 isbWon board value = any and (winbpos board value)
 
 winbpos :: Board -> Value -> [[Bool]]
-winbpos board value = map (map (\x -> Map.lookup x board == Just (value, _)) ) (rwinpos ++ cwinpos ++ dwinpos ++ adwinpos)
+winbpos board value = map (map (\p -> grideq value (Map.lookup p board))) (rwinpos ++ cwinpos ++ dwinpos ++ adwinpos)
 
-rbwinpos :: [[Position]]
+grideq :: Value -> Maybe (GridStatus, Grid) -> Bool
+grideq _ Nothing = False
+grideq value gridT   = Win value == gridStatus
+                      where
+                        gridStatus = fst (fromJust gridT)
+
+rbwinpos :: [[Int]]
 rbwinpos = [[3*i+j+1 | i <- [0, 1, 2]] | j <- [0, 1, 2]]
 
-cbwinpos :: [[Position]]
+cbwinpos :: [[Int]]
 cbwinpos = [[3*i+j+1 | j <- [0, 1, 2]] | i <- [0, 1, 2]]
 
-dbwinpos :: [[Position]]
+dbwinpos :: [[Int]]
 dbwinpos = [[4*i+1| i <- [0, 1, 2]]]
 
-adbwinpos :: [[Position]]
+adbwinpos :: [[Int]]
 adbwinpos = [[2*i+3| i <- [0, 1, 2]]]
 
 ----------------------------------
 ---- / Moves
 ----------------------------------
 
-moveb :: (Int -> Int) -> Position -> Position
-moveb dir pos | dir snd pos == snd pos = (dir fst pos, 1)
-              | otherwise              = (fst pos, dir snd pos)
+moveb :: (Int -> Int) -> CurPos -> CurPos
+moveb dir pos | dir (snd pos) == snd pos = (dir (fst pos), 1)
+              | otherwise              = (fst pos, dir (snd pos))
